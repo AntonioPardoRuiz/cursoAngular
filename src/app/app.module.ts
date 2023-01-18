15bc06f114cb0f7eb4f338d1ff61571c12d1fccb
Antonio Miguel Pardo Ruiz <a2161@inycom.es>
2023-01-18 13:13:56 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Importamos el boton app.module.ts
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
