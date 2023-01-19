1ba244ed4c45fdd8db4360d628abf4e75044e0e4
Antonio Miguel Pardo Ruiz <a2161@inycom.es>
2023-01-19 10:31:33 +0100

    Cambios en el proceso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 //Importamos el boton del framework 
 import {ToolbarModule} from 'primeng/toolbar';
-import {MenuItem, PrimeIcons} from 'primeng/api';
-import { PrimeNGConfig } from 'primeng/api';
+
 
 
 @Component({
@@ -11,13 +10,11 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private primengConfig: PrimeNGConfig) {}
 
   title = 'angularinycom';
 //Definimos el navegador del menu 
   //Al inicio cargame estos elemenos
   ngOnInit() {
-    this.primengConfig.ripple = true;
   }
   
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } 
+    from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +19,7 @@ import {ToolbarModule} from 'primeng/toolbar';
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
+    BrowserAnimationsModule,
     ToolbarModule,
 
   ],
