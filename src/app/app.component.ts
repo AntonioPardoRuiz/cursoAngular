2ad711ab648a7e67100c3bff7a812305550b61c3
Antonio Miguel Pardo Ruiz <a2161@inycom.es>
2023-01-24 13:26:38 +0100

    Añadimos control de Inputs y Outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,25 @@ import { NavegadorComponent } from './navegador/navegador.component';
 export class AppComponent {
 
   title = 'angularinycom';
+  miapp ="MESASILA";
+  nuevoTitulo:string='';
+  contacto:string='CONTACTO';
+  blog:string='BLOG';
+  mesasila:string='MESASILA MUNDIAL';
+  fechaDia:string='';
+ 
+
 //Definimos el navegador del menu 
   //Al inicio cargame estos elemenos
   ngOnInit() {
+    console.log("Inicio del componente App");
+    //Calcular fecha del dia 
+    const fecha = new Date();
+    this.fechaDia = fecha.toLocaleDateString();
   }
   
+  addNuevoTitulo(valor:string){
+    this.nuevoTitulo = valor;
+  }
 
 }
--- src/app/navegador/navegador.component.ts
+++ src/app/navegador/navegador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 //Importamos las imagenes
 
 @Component({
@@ -8,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavegadorComponent implements OnInit {
 
+  //Recibimos los valores de entrada del proceso. 
+  @Input() titulo = '';
+  @Input() contacto ='';
+  @Input() blog ='';
+  @Input() mesasila ='';
+  @Input() fechaDia='';
+
+  @Output() nuevoTitulo = new EventEmitter<string>();
+  value:string="Registrese"
+
   constructor() { }
 
+
   ngOnInit(): void {
+    console.log("Inicio del componente Navegador");
+  }
+
+  //Creamos una funcion de tipo click, para pasar los valores
+  addNuevoTitulo(){
+    //Emitimos el nuevo valor
+    this.nuevoTitulo.emit(this.value);
   }
 
 }
